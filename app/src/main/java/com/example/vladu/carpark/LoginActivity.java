39d36f7532304c054c42ddbcd0efc3c2db471f34
VladAndreescu <andreesv@uni.coventry.ac.uk>
2018-11-06 17:17:36 +0000

    created register activity

--- app/src/main/java/com/example/vladu/carpark/LoginActivity.java
+++ app/src/main/java/com/example/vladu/carpark/LoginActivity.java
@@ -1,7 +1,11 @@
 package com.example.vladu.carpark;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -9,5 +13,18 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        final EditText usernameLogin = (EditText) findViewById(R.id.usernameEdt);
+        final EditText passwordLogin = (EditText) findViewById(R.id.passwordEdt);
+        final Button registerLink = (Button) findViewById(R.id.registerLinkBtn);
+        final Button login = (Button) findViewById(R.id.loginBtn);
+
+        registerLink.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent registerIntent = new Intent(LoginActivity.this,RegisterActivity.class);
+                LoginActivity.this.startActivity(registerIntent);
+            }
+        });
     }
 }
