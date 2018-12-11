da87a21fb1cbbee3ab04dd384bd4c4641eba661a
VladAndreescu <andreesv@uni.coventry.ac.uk>
2018-12-11 01:57:04 +0000

    integrating postcode api for the location section

--- app/src/main/java/com/example/vladu/carpark/LocationActivity.java
+++ app/src/main/java/com/example/vladu/carpark/LocationActivity.java
@@ -1,21 +1,39 @@
 package com.example.vladu.carpark;
+
+import android.Manifest;
+import android.app.ProgressDialog;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.Toast;
+
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.GoogleMap.OnMyLocationButtonClickListener;
 import com.google.android.gms.maps.GoogleMap.OnMyLocationClickListener;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 
-import android.Manifest;
-import android.content.pm.PackageManager;
-import android.location.Location;
-import android.os.Bundle;
-import android.support.annotation.NonNull;
-import android.support.v4.app.ActivityCompat;
-import android.support.v4.content.ContextCompat;
-import android.support.v7.app.AppCompatActivity;
-import android.widget.EditText;
-import android.widget.Toast;
+import org.json.JSONArray;
+import org.json.JSONObject;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.util.ArrayList;
 
 public class LocationActivity extends AppCompatActivity
@@ -33,6 +51,15 @@ public class LocationActivity extends AppCompatActivity
 
     private GoogleMap mMap;
 
+    EditText postcodeEdit;
+    EditText townEdit;
+    Button getAddresses;
+    JSONObject jsonObject;
+    JSONArray jsonArray;
+    ArrayList<String> addresses;
+    ProgressDialog pd;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -42,10 +69,115 @@ public class LocationActivity extends AppCompatActivity
         mapFragment.getMapAsync(this);
 
         //declaring variables
-        final EditText postcodeEdit = (EditText) findViewById(R.id.postcodeEdit);
-        final EditText streetEdit = (EditText) findViewById(R.id.streetEdit);
-        final EditText townEdit = (EditText) findViewById(R.id.townEdit);
+        postcodeEdit = (EditText) findViewById(R.id.postcodeEdit);
+        townEdit = (EditText) findViewById(R.id.townEdit);
+        getAddresses = (Button) findViewById(R.id.findAddressBtn);
+
+        getAddresses.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                new getJsonData().execute("https://api.getAddress.io/find/" + postcodeEdit.getText().toString() + "?api-key=Q8BXrVkS7EGLaUmdNH8lIw16531");
+            }
+        });
+
+
     }
+
+    private class getJsonData extends AsyncTask<String, String, String> {
+
+        protected void onPreExecute() {
+            super.onPreExecute();
+
+            pd = new ProgressDialog(LocationActivity.this);
+            pd.setMessage("Please wait");
+            pd.setCancelable(false);
+            pd.show();
+        }
+
+        protected String doInBackground(String... params) {
+
+
+            HttpURLConnection connection = null;
+            BufferedReader reader = null;
+
+            try {
+                URL url = new URL(params[0]);
+                connection = (HttpURLConnection) url.openConnection();
+                connection.connect();
+
+
+                InputStream stream = connection.getInputStream();
+
+                reader = new BufferedReader(new InputStreamReader(stream));
+
+                StringBuffer buffer = new StringBuffer();
+                String line = "";
+
+                while ((line = reader.readLine()) != null) {
+                    buffer.append(line + "\n");
+                    Log.d("Response: ", "> " + line);   //here u ll get whole response...... :-)
+
+
+
+                }
+                jsonArray = new JSONArray(line);
+                for(int i = 0; i < jsonArray.length(); i++ ){
+                    jsonObject = jsonArray.getJSONObject(i);
+                    Log.d("Output", jsonObject.getString("addresses"));
+                }
+
+
+                return buffer.toString();
+
+            }catch(Exception e){
+                Log.e("Error", e.getMessage());
+            } finally {
+                if (connection != null) {
+                    connection.disconnect();
+                }
+                try {
+                    if (reader != null) {
+                        reader.close();
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+            super.onPostExecute(result);
+            if (pd.isShowing()){
+                pd.dismiss();
+            }
+
+           Spinner addressSpinner = (Spinner) findViewById(R.id.addressSpinner);
+            String[] cacat = {"cacat1", "cacat2"};
+            addressSpinner.setAdapter(new ArrayAdapter<String>(LocationActivity.this,
+                    android.R.layout.simple_spinner_dropdown_item,cacat));
+
+            addressSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+                @Override
+                public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+
+                }
+
+                @Override
+                public void onNothingSelected(AdapterView<?> parent) {
+
+                }
+            });
+
+
+
+        }
+    }
+
+
+
+
 
     @Override
     public void onMapReady(GoogleMap map) {
@@ -120,3 +252,4 @@ public class LocationActivity extends AppCompatActivity
     }
 
 }
+
